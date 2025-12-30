388ddef72238215816a19e05afc06feac421879f
Vahe Hasratyan <vahehasratyan@gmail.com>
2025-12-30 17:40:41 +0400

    Changes

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useLanguage } from "@/components/language-provider";
+import Images from "next/image";
 
 export default function Footer() {
   const { locale, t } = useLanguage();
@@ -17,6 +18,10 @@ export default function Footer() {
           •
           <Link href={"https://b2b.megatours.am"} target={"_blank"}>{t.footer.b2bPartnership}</Link>
         </nav>
+        <div className="social"></div>
+        <div className="payment">
+          <Images src="/images/icons/payment-methods.png" width="350" height="50" unoptimized alt="Payment methods" />
+        </div>
       </div>
     </footer>
   );
