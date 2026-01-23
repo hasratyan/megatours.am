e006364ca7b9ede0ca62b74e57c6b3b8d5b69d1f
Vahe Hasratyan <vahehasratyan@gmail.com>
2026-01-23 13:07:07 +0400

    Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next";
 import * as React from "react";
 import { GoogleTagManager } from '@next/third-parties/google'
 import { Google_Sans } from "next/font/google";
-import localFont from "next/font/local";
+import 'material-symbols';
 import Providers from "@/components/providers";
 import { metadataBase } from "@/lib/metadata";
 import "./globals.css";
@@ -11,13 +11,6 @@ const body = Google_Sans({
   subsets: ["latin"],
   variable: "--font-body",
   weight: ["400", "500", "600", "700"],
-  display: "swap",
-});
-
-const materialSymbolsRounded = localFont({
-  src: "../public/fonts/material-symbols-rounded.woff2",
-  variable: "--font-icons",
-  weight: "100 700",
   display: "swap",
 });
 
@@ -39,7 +32,7 @@ export default function RootLayout({children,}: Readonly<{
 }>) {
   return (
     <html suppressHydrationWarning>
-    <body className={`${body.variable} ${materialSymbolsRounded.variable} antialiased`}>
+    <body className={`${body.variable} antialiased`}>
     <GoogleTagManager gtmId="GTM-MQJD3BQN" />
       <Providers>{children}</Providers>
     </body>
