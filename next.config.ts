0eb8e1739135c8a052da091245470ca306637192
Vahe Hasratyan <vahehasratyan@gmail.com>
2026-01-30 15:02:14 +0400

    Changes

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
       {
